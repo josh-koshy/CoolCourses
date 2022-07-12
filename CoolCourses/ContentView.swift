1a762edc45b6b51ad79eec7622eca7bb406620dc
Joshua Koshy <89991793+josh-koshy@users.noreply.github.com>
2022-07-12 14:37:51 -0400

    Added a gitignore and initalized XCode project.

---
+++ CoolCourses/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CoolCourses
+//
+//  Created by Joshua Koshy on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoolCourses/CoolCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolCoursesApp.swift
+//  CoolCourses
+//
+//  Created by Joshua Koshy on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
